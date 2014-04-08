a65697a0f4ab0a3f9716235c1270815f9423c5e0
Stefan Bunk <stefan.bunk@googlemail.com>
2014-04-08 17:04:31 +0200

    Fixed programming stub to actually compile.

--- src/main/java/de/hpi/krestel/mySearchEngine/SearchEngineTest.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/SearchEngineTest.java
@@ -21,7 +21,7 @@ public class SearchEngineTest {
 	public static void main(String[] args) {
 
 		// Get a new search engine
-		SearchEngine se1 = new SearchEngineX();
+		SearchEngine se1 = new SearchEngineY();
 		evaluate(se1);
 		// SearchEngine se2 = new SearchEngineY();
 		// evaluate(se2);
