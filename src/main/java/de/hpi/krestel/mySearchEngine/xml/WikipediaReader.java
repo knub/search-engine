3ea972e17d7a31f306d261e081d3b2db151a35c8
Stefan Bunk <stefan.bunk@googlemail.com>
2014-04-09 12:07:20 +0200

    Added WikipediaReader.

---
+++ src/main/java/de/hpi/krestel/mySearchEngine/xml/WikipediaReader.java
@@ -0,0 +1,12 @@
+package de.hpi.krestel.mySearchEngine.xml;
+
+public class WikipediaReader {
+
+	public static void readXMLWithSAX() {
+		System.out.println("SAX");
+	}
+
+	public static void readXMLWithStAX() {
+		System.out.println("StAX");
+	}
+}
