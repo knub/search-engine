7b13c99d57458f34ba4f25a9036ec7d09518950e
Franz Liedke <franz.liedke@student.hpi.uni-potsdam.de>
2014-04-15 16:36:24 +0200

    Get rid of one line. Thanks, Stefan!

--- src/main/java/de/hpi/krestel/mySearchEngine/processing/Pipeline.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/processing/Pipeline.java
@@ -1,13 +1,12 @@
 package de.hpi.krestel.mySearchEngine.processing;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
 
 public class Pipeline extends Vector<ProcessorInterface> {
     public List<String> process(String input) {
-        List<String> list = new ArrayList<String>();
-        list.add(input);
+        List<String> list = Arrays.asList(input);
 
         for (ProcessorInterface processor : this) {
             list = processor.process(list);
