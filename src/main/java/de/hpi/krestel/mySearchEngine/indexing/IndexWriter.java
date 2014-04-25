9adb3f45a7fcb9b7a66fdd7b43eef30e3cab5724
Stefan Bunk <stefan.bunk@googlemail.com>
2014-04-25 14:37:32 +0200

    Refactored IndexWriter.

--- src/main/java/de/hpi/krestel/mySearchEngine/indexing/IndexWriter.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/indexing/IndexWriter.java
@@ -1,8 +1,10 @@
 package de.hpi.krestel.mySearchEngine.indexing;
 
+import de.hpi.krestel.mySearchEngine.domain.DocumentEntry;
 import de.hpi.krestel.mySearchEngine.domain.OccurrenceMap;
 import de.hpi.krestel.mySearchEngine.util.stream.Bit23Writer;
 import de.hpi.krestel.mySearchEngine.util.stream.BitOutputStream;
+import de.hpi.krestel.mySearchEngine.util.stream.EliasGammaWriter;
 
 import java.io.*;
 import java.util.Arrays;
@@ -10,36 +12,68 @@ import java.util.Map;
 
 public class IndexWriter {
 
+	// the base stream for all other streams
+	private BitOutputStream bos = null;
+
+	// for elias gamma ints
+	private EliasGammaWriter eliasGammaWriter = null;
+	// for strings
+	private PrintStream ps = null;
+	// for 23-bits
+	private Bit23Writer bit23writer = null;
+
+	// counter for naming the index files
 	private static int indexCounter = 0;
-	public String write(Map<String, OccurrenceMap> partIndex) {
+
+	public IndexWriter() {
 		try {
-			String fileName = nextFileName();
-			BitOutputStream bos = new BitOutputStream(new FileOutputStream(fileName));
-			Bit23Writer bit23writer = new Bit23Writer(bos);
-			PrintStream ps = new PrintStream(bos);
+			String fileName  = nextFileName();
+			bos              = new BitOutputStream(new FileOutputStream(fileName));
+			bit23writer      = new Bit23Writer(bos);
+			eliasGammaWriter = new EliasGammaWriter(bos);
+			ps               = new PrintStream(bos);
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void write(Map<String, OccurrenceMap> partIndex) {
+		try {
 			int i = 0;
 			for (Map.Entry<String, OccurrenceMap> entry : partIndex.entrySet()) {
-				ps.print(entry.getKey());
-				System.out.println("Word: " + entry.getKey());
-				bos.write(new byte[] { 0 });
-				OccurrenceMap occurrenceMap = entry.getValue();
-				int[] keys = occurrenceMap.keys();
-				Arrays.sort(keys);
-				bit23writer.write(keys[0]);
-				System.out.println("Document-ID: " + keys[0]);
+				writeIndexWord(entry.getKey());
+				writeDocumentEntries(entry.getValue());
 				if (i == 4) break;
 				i++;
 			}
 			bos.close();
-			bit23writer.close();
-			ps.close();
-
-
-			return fileName;
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 
+	}
+
+	private void writeDocumentEntries(OccurrenceMap occurrenceMap) throws IOException {
+		int[] keys = occurrenceMap.keys();
+		Arrays.sort(keys);
+
+		for (int key : keys) {
+			bit23writer.write(key);
+			writeDocumentEntry(occurrenceMap.get(key));
+		}
+
+		System.out.println("Document-ID: " + keys[0]);
+	}
+
+	private void writeDocumentEntry(DocumentEntry documentEntry) throws IOException {
+		eliasGammaWriter.write(documentEntry.size());
+
+	}
+
+	private void writeIndexWord(String indexWord) throws IOException {
+		ps.print(indexWord);
+		bos.write(new byte[] { 0 });
+		System.out.println("Word: " + indexWord);
 	}
 
 	private String nextFileName() {
