c8de9340d26398547e43faa2a46e44338d848767
Franz Liedke <franz.liedke@student.hpi.uni-potsdam.de>
2014-04-25 17:31:09 +0200

    Implement merge() in WordMap instead of OccurrenceMap.

--- src/main/java/de/hpi/krestel/mySearchEngine/domain/WordMap.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/domain/WordMap.java
@@ -1,7 +1,5 @@
 package de.hpi.krestel.mySearchEngine.domain;
 
-import org.omg.CORBA.Environment;
-
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -18,4 +16,17 @@ public class WordMap extends TreeMap<String, OccurrenceMap> {
 		sb.append("}");
 		return sb.toString();
 	}
+
+    public void merge(WordMap otherMap, String word) throws Exception {
+        if (this.keySet().removeAll(otherMap.keySet())) {
+            throw new Exception("Can only merge occurrence maps with distinct key sets");
+        }
+
+        OccurrenceMap map1 = this.get(word);
+        OccurrenceMap map2 = otherMap.get(word);
+
+        map1.putAll(map2);
+        map2.clear();
+    }
+
 }
