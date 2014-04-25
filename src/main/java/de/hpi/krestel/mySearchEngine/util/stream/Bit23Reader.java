2a7d97cdc6fa1ffa7489b5378f0f87b05126c9f0
Stefan Bunk <stefan.bunk@googlemail.com>
2014-04-25 12:47:15 +0200

    Added the Bit23Reader and -Writer.

--- src/main/java/de/hpi/krestel/mySearchEngine/util/stream/Bit23Reader.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/util/stream/Bit23Reader.java
@@ -1,4 +1,23 @@
 package de.hpi.krestel.mySearchEngine.util.stream;
 
-public class Bit23Reader {
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Bit23Reader extends InputStream {
+
+	private final BitInputStream bis;
+
+	public Bit23Reader(BitInputStream bis) {
+		this.bis = bis;
+	}
+
+	@Override
+	public int read() throws IOException {
+		int number = 0;
+		for (int i = 1; i <= 23; i++) {
+			int readBit = bis.readBit();
+			number = number * 2 + readBit;
+		}
+		return number;
+	}
 }
--- src/main/java/de/hpi/krestel/mySearchEngine/util/stream/Bit23Writer.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/util/stream/Bit23Writer.java
@@ -1,4 +1,23 @@
 package de.hpi.krestel.mySearchEngine.util.stream;
 
-public class Bit23Writer {
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Bit23Writer extends OutputStream {
+
+	private final BitOutputStream bos;
+
+	public Bit23Writer(BitOutputStream bos) {
+		this.bos = bos;
+	}
+	@Override
+	public void write(int number) throws IOException {
+		for (int i = 22; i >= 0; i--) {
+			bos.writeBit(getBit(number, i));
+		}
+	}
+
+	int getBit(int n, int k) {
+		return (n >> k) & 1;
+	}
 }
