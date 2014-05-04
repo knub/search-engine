e69a26419087b7f63676f0719d53344cb1c26329
Stefan Bunk <stefan.bunk@googlemail.com>
2014-05-04 19:52:06 +0200

    Determining common documents in phrase search.

--- src/main/java/de/hpi/krestel/mySearchEngine/searching/query/operators/Phrase.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/searching/query/operators/Phrase.java
@@ -4,8 +4,12 @@ import de.hpi.krestel.mySearchEngine.domain.OccurrenceMap;
 import de.hpi.krestel.mySearchEngine.searching.IndexSearcher;
 import de.hpi.krestel.mySearchEngine.searching.query.Operator;
 import de.hpi.krestel.mySearchEngine.searching.ResultSet;
+import gnu.trove.set.TIntSet;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class Phrase implements Operator {
 
@@ -17,11 +21,28 @@ public class Phrase implements Operator {
 
     @Override
     public OccurrenceMap evaluate(IndexSearcher searcher) {
+	    List<OccurrenceMap> singleResults = new ArrayList<OccurrenceMap>();
+
 	    for (String word : phrase) {
-//		    System.out.println("PhraseWord: " + word);
 		    OccurrenceMap result = searcher.search(word);
-//		    System.out.println(result);
+		    singleResults.add(result);
+		    System.out.println("==========" + word + "=============");
+		    System.out.println(result);
 	    }
+	    int[] commonDocuments = determineCommonDocuments(singleResults);
+	    System.out.println("==============Common Documents===============");
+	    System.out.println(Arrays.toString(commonDocuments));
+
 	    return null;
     }
+
+	private int[] determineCommonDocuments(List<OccurrenceMap> singleResults) {
+		TIntSet commonKeys = singleResults.get(0).keySet();
+		Iterator<OccurrenceMap> it = singleResults.iterator();
+		// skip first
+		it.next();
+		while (it.hasNext())
+			commonKeys.retainAll(it.next().keySet());
+		return commonKeys.toArray();
+	}
 }
