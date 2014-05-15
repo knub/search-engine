3ce0d4fe90a19b7f9d4a4b8935d540d6d6d7fc85
Stefan Bunk <stefan.bunk@googlemail.com>
2014-05-15 15:29:37 +0200

    Nice highlighting of search results.

--- src/main/java/de/hpi/krestel/mySearchEngine/searching/SnippetReader.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/searching/SnippetReader.java
@@ -26,12 +26,18 @@ public class SnippetReader {
 			// one long is 8 bytes, so we have to multiply by 8
 			offsets.seek(docId * 8);
 			long offset = offsets.readLong();
-//			texts.seek(offset + inFileOffset);
-			texts.seek(Math.max(offset + inFileOffset - SNIPPET_LENGTH / 2, 0));
+			long seekStart = Math.max(offset + inFileOffset - SNIPPET_LENGTH / 2, 0);
+			int searchBeforeLength = (int) (offset + inFileOffset - seekStart);
+			texts.seek(seekStart);
+
 			byte[] text = new byte[SNIPPET_LENGTH * 2];
 			texts.read(text);
 			String snippet = new String(text);
 			snippet = snippet.replace("\n", " ").replace("\0", "");
+//			String setPlainText = "\033[0;0m";
+			snippet = snippet.substring(0, searchBeforeLength) +
+					"\033[4m" + snippet.substring(searchBeforeLength, searchBeforeLength + length) + "\033[0;m" +
+					snippet.substring(searchBeforeLength + length, snippet.length());
 			return snippet.substring(0, Math.min(SNIPPET_LENGTH, snippet.length()));
 		} catch (IOException e) {
 			throw new RuntimeException(e);
