8dbb3f5a975b0de135c897f0935dd89d19c6dcc6
Franz Liedke <franz@develophp.org>
2014-09-18 18:03:39 +0200

    Clean up and comment the indexer.

--- src/main/java/de/hpi/krestel/mySearchEngine/indexing/Indexer.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/indexing/Indexer.java
@@ -19,7 +19,8 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Indexer implements TextCompletedListener {
+public class Indexer implements TextCompletedListener
+{
 
 	private final Pipeline preprocessingPipeline = Pipeline.createPreprocessingPipeline();
     private final WordMap partIndex = new WordMap();
@@ -38,27 +39,41 @@ public class Indexer implements TextCompletedListener {
 
 	private Map<String, StringBuilder> links = new HashMap<String, StringBuilder>();
 
-    public Indexer(String directory) {
+    public Indexer(String directory)
+    {
 	    this.directory = directory;
         this.titleMap = new ArrayList<String>();
     }
 
-	public void run() {
+	public void run()
+    {
 		System.out.println("INDEXING");
+
 		docCount = 0;
 		cumulatedDocLength = 0;
+
+        // Set up the parser and make sure we're notified every time a
 		WikipediaReader reader = new WikipediaReader();
 		reader.addTextCompletedListener(this);
-		reader.readWikiFile();
-		writePartIndex();
-		preprocessingPipeline.finished();
 
-        triggerMergingProcess();
-		writeSeekList();
-		writeLinkList();
+        // Parse the Wiki entries and write the partial indexes
+        reader.readWikiFile();
+		this.writePartIndex();
+
+        // Do some cleanup in the processing pipeline
+		this.preprocessingPipeline.finished();
+
+        // Merge the partial indices, write out seek list and list of links
+        this.triggerMergingProcess();
+		this.writeSeekList();
+		this.writeLinkList();
 	}
 
-	private void writeSeekList() {
+    /**
+     * Write the seek list to a file.
+     */
+	private void writeSeekList()
+    {
         seekList.setTitleMap(this.titleMap);
 		try {
 			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(directory + "/seek_list"));
@@ -70,7 +85,11 @@ public class Indexer implements TextCompletedListener {
 		}
 	}
 
-	private void writeLinkList() {
+    /**
+     * Write the list of links to a file.
+     */
+	private void writeLinkList()
+    {
 		try {
 			PrintWriter printWriter = new PrintWriter(directory + "/links");
 			for (Map.Entry<String, StringBuilder> key : links.entrySet())
@@ -79,12 +98,19 @@ public class Indexer implements TextCompletedListener {
 		} catch (FileNotFoundException e) {
 			throw new RuntimeException(e);
 		}
-
 	}
 
+    /**
+     * Handle a parsed wiki document.
+     *
+     * @param text
+     * @param title
+     */
 	@Override
-	public void onTextCompleted(String text, String title) {
-		if (createLinkConnections) {
+	public void onTextCompleted(String text, String title)
+    {
+        // Parse the links, too
+		if (this.createLinkConnections) {
 			Pattern pattern = Pattern.compile("\\[\\[(.*?)\\]\\]");
 			Matcher matcher = pattern.matcher(text);
 			while (matcher.find()) {
@@ -115,72 +141,115 @@ public class Indexer implements TextCompletedListener {
 			}
 		}
 
+        // Tokenize and preprocess the document
 		List<CoreLabel> labels = preprocessingPipeline.start(text);
-		docCount += 1;
-		cumulatedDocLength += labels.size();
-		docLengths.put(documentId, labels.size());
-//		System.out.println("Title: " + title + ", Document-ID: " + documentId);
-		indexText(labels);
-        this.titleMap.add(title);
-        documentId++;
-		long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-		long freeMemory = Runtime.getRuntime().maxMemory() - usedMemory;
-		if (documentId % 1000 == 0) {
-			if (startTime != 0)
-			System.out.println("Dokument-ID: " + documentId + ". Last 1000 took: " + (System.currentTimeMillis() - startTime) + " ms");
-			startTime = System.currentTimeMillis();
-		}
-		if (freeMemory / 1024 / 1024 < 400) {
-			writePartIndex();
-			System.out.print("Garbage collect ..");
-			for (int i = 0; i < 10; i++)
-				System.gc();
-			System.out.println("Done.");
-		}
-	}
 
-	public void indexText(List<CoreLabel> labels) {
+
+//		System.out.println("Title: " + title + ", Document-ID: " + documentId);
+        // Index the tests
+        this.indexText(labels);
+        this.titleMap.add(title);
+
+        this.watchMemory();
+
+        // Gather some statistics
+        this.docCount += 1;
+        this.documentId += 1;
+        this.cumulatedDocLength += labels.size();
+        this.docLengths.put(this.documentId, labels.size());
+    }
+
+    protected void watchMemory()
+    {
+        // Gather memory info
+        long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        long freeMemory = Runtime.getRuntime().maxMemory() - usedMemory;
+
+        // Log statistics
+        if (this.documentId % 1000 == 0) {
+            long now = System.currentTimeMillis();
+            if (this.documentId > 0) {
+                System.out.println("Dokument-ID: " + this.documentId + ". Last 1000 took: " + (now - this.startTime) + " ms");
+            }
+            this.startTime = now;
+        }
+
+        // Run the garbage collector if necessary
+        if (freeMemory / 1024 / 1024 < 400) {
+            // Dump the current partial index
+            this.writePartIndex();
+
+            System.out.print("Garbage collect ..");
+            for (int i = 0; i < 10; i++) {
+                System.gc();
+            }
+            System.out.println("Done.");
+        }
+    }
+
+	public void indexText(List<CoreLabel> labels)
+    {
+        // Run through the results...
         for (int position = 0; position < labels.size(); position++) {
             CoreLabel label = labels.get(position);
-            if (partIndex.containsKey(label.value())) {
-                OccurrenceMap occurrenceMap = partIndex.get(label.value());
-                if (occurrenceMap.containsKey(documentId)) {
-                    DocumentEntry documentEntry = occurrenceMap.get(documentId);
-                    documentEntry.positions.add(position);
-                    documentEntry.offsets.add(label.beginPosition());
-                    documentEntry.lengths.add(label.endPosition() - label.beginPosition());
-                } else {
-                    DocumentEntry documentEntry = new DocumentEntry(position, label.beginPosition(),
-                            label.endPosition() - label.beginPosition());
-                    occurrenceMap.put(documentId, documentEntry);
-                }
+
+            OccurrenceMap occurrenceMap;
+            DocumentEntry documentEntry;
+
+            // Fetch or create the occurrence map
+            if (this.partIndex.containsKey(label.value())) {
+                occurrenceMap = this.partIndex.get(label.value());
             } else {
-                DocumentEntry documentEntry = new DocumentEntry(position, label.beginPosition(),
-                        label.endPosition() - label.beginPosition());
-                OccurrenceMap occurrenceMap = new OccurrenceMap();
-                occurrenceMap.put(documentId, documentEntry);
-                partIndex.put(label.value(), occurrenceMap);
+                occurrenceMap = new OccurrenceMap();
+                this.partIndex.put(label.value(), occurrenceMap);
             }
+
+            // Fetch or create the document entry
+            if (occurrenceMap.containsKey(this.documentId)) {
+                documentEntry = occurrenceMap.get(this.documentId);
+            } else {
+                documentEntry = new DocumentEntry();
+                occurrenceMap.put(this.documentId, documentEntry);
+            }
+
+            // And finally, store the current position
+            documentEntry.positions.add(position);
+            documentEntry.offsets.add(label.beginPosition());
+            documentEntry.lengths.add(label.endPosition() - label.beginPosition());
         }
 	}
 
-    public void writePartIndex() {
-        IndexWriter indexWriter = new IndexWriter(directory);
-        indexWriter.write(partIndex);
+    /**
+     * Write the current part index.
+     */
+    public void writePartIndex()
+    {
+        // Write out the current part index to a file
+        IndexWriter indexWriter = new IndexWriter(this.directory);
+        indexWriter.write(this.partIndex);
         indexWriter.close();
-        partIndexFileNames.add(indexWriter.getFileName());
-        partIndex.clear();
+
+        // Store the filename
+        this.partIndexFileNames.add(indexWriter.getFileName());
+
+        // Reset the part index so that we're ready for the next round
+        this.partIndex.clear();
         System.gc();
     }
 
-    public void triggerMergingProcess() {
+    public void triggerMergingProcess()
+    {
+        // Create reader instances for all our part indices
         List<IndexReader> indexReaders = new ArrayList<IndexReader>(preprocessingPipeline.size());
         for (String partIndexFileName : partIndexFileNames) {
             indexReaders.add(new IndexReader(partIndexFileName));
         }
 
+        // Our merger needs access to a writer and all of our readers
         IndexWriter indexWriter = new IndexWriter(directory, "final_index", true);
         IndexMerger indexMerger = new IndexMerger(indexReaders, indexWriter);
+
+        // Run the merge!
         try {
             indexMerger.merge();
         }
@@ -188,17 +257,22 @@ public class Indexer implements TextCompletedListener {
             System.out.println("Something bad happened at merging time:");
             System.out.println(e.getLocalizedMessage());
         }
-        indexFilename = indexWriter.getFileName();
-        seekList = indexWriter.getSeekList();
-	    seekList.setDocLengths(docLengths);
-	    seekList.setDocumentCount(docCount);
+
+        this.indexFilename = indexWriter.getFileName();
+
+        // Extract the seek list
+        this.seekList = indexWriter.getSeekList();
+	    this.seekList.setDocLengths(this.docLengths);
+	    this.seekList.setDocumentCount(this.docCount);
     }
 
-    public String getIndexFilename() {
-        return indexFilename;
+    public String getIndexFilename()
+    {
+        return this.indexFilename;
     }
 
-    public SeekList getSeekList() {
-        return seekList;
+    public SeekList getSeekList()
+    {
+        return this.seekList;
     }
 }
