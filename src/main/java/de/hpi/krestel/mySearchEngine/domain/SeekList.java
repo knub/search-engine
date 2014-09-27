120d4d18524737f2d3346f26e5ff5a8ec18fa2a2
Franz Liedke <franz@develophp.org>
2014-09-27 13:30:05 +0200

    Clean up in seek list class.

--- src/main/java/de/hpi/krestel/mySearchEngine/domain/SeekList.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/domain/SeekList.java
@@ -2,18 +2,21 @@ package de.hpi.krestel.mySearchEngine.domain;
 
 import java.util.TreeMap;
 
-public class SeekList extends TreeMap<String, Long> {
+public class SeekList extends TreeMap<String, Long>
+{
     //TODO: PLZ write me incrementally to a file, read me from there
     // see Documents
 
     // remove document handling from here; it should use own file
     private Documents documents;
 
-    public Documents getDocuments() {
+    public Documents getDocuments()
+    {
         return documents;
     }
 
-    public void setDocuments(Documents documents) {
+    public void setDocuments(Documents documents)
+    {
         this.documents = documents;
     }
 
