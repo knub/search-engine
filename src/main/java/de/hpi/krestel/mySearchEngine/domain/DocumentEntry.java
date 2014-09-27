06ef3d3e629505fbb6eec56a58dd3e82e111fe52
Franz Liedke <franz@develophp.org>
2014-09-27 12:54:36 +0200

    Clean up DocumentEntry code.

--- src/main/java/de/hpi/krestel/mySearchEngine/domain/DocumentEntry.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/domain/DocumentEntry.java
@@ -3,46 +3,61 @@ package de.hpi.krestel.mySearchEngine.domain;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
 
-public class DocumentEntry {
+public class DocumentEntry
+{
     public TIntList positions = new TIntArrayList();
     public TIntList offsets = new TIntArrayList();
     public TIntList lengths = new TIntArrayList();
 
 	double rank = -1;
 
-	public DocumentEntry() {}
-    public DocumentEntry(int position, int offset, int length) {
-        positions.add(position);
-        offsets.add(offset);
-        lengths.add(length);
+    public DocumentEntry() {}
+
+    public DocumentEntry(int position, int offset, int length)
+    {
+        this.positions.add(position);
+        this.offsets.add(offset);
+        this.lengths.add(length);
     }
 
-	public DocumentEntry(int[] position, int offset[], int length[]) {
-		positions.add(position);
-		offsets.add(offset);
-		lengths.add(length);
+	public DocumentEntry(int[] position, int offset[], int length[])
+    {
+		this.positions.add(position);
+		this.offsets.add(offset);
+		this.lengths.add(length);
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+    {
 		StringBuilder sb = new StringBuilder();
-		sb.append(positions.size() + ",");
-		for (int i = 0; i < positions.size(); i++) {
-			sb.append("[" + positions.get(i) + "," + offsets.get(i) + "," + lengths.get(i) + "]");
+
+		sb.append(this.positions.size()).append(",");
+		for (int i = 0; i < this.positions.size(); i++) {
+			sb.append("[")
+              .append(this.positions.get(i))
+              .append(",")
+              .append(this.offsets.get(i))
+              .append(",")
+              .append(this.lengths.get(i))
+              .append("]");
 		}
+
 		return sb.toString();
 	}
 
-	public int size() {
-		return positions.size();
+	public int size()
+    {
+		return this.positions.size();
 	}
 
-	public Double getRank() {
-		return rank;
+	public Double getRank()
+    {
+		return this.rank;
 	}
 
-	public void setRank(double rank) {
+	public void setRank(double rank)
+    {
 		this.rank = rank;
 	}
-
 }
