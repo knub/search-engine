855eb30975cc4b0c44ba4e3fd7ce5b5cc2bea235
Franz Liedke <franz@develophp.org>
2014-10-01 16:28:23 +0200

    Get PrefixedWord to compile again for now.

--- src/main/java/de/hpi/krestel/mySearchEngine/searching/query/operators/PrefixedWord.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/searching/query/operators/PrefixedWord.java
@@ -31,12 +31,13 @@ public class PrefixedWord extends UnaryOperator implements Operator {
 
     @Override
     public OccurrenceMap evaluate(IndexSearcher searcher) {
+        /*
 	    String currentKey = searcher.getSeekList().ceilingKey(word);
-	    OccurrenceMap result = new OccurrenceMap();
-	    while (currentKey.startsWith(word)) {
+	    */OccurrenceMap result = new OccurrenceMap();
+	    /*while (currentKey.startsWith(word)) {
 		    result.merge(searcher.search(currentKey));
 		    currentKey = searcher.getSeekList().higherKey(currentKey);
-	    }
+	    }*/
 	    return result;
     }
 }
